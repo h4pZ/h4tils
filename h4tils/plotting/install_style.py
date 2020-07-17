b86cfe0e2e195b9c9dd689e06aedcfecd90f1cca
h4pZ <h4pz@protonmail.com>
2020-07-17 22:17:07 +0200

    Add Matplotlib and Plotly plotting utils

---
+++ h4tils/plotting/install_style.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import os
+import sys
+from shutil import copy
+
+
+def install_hibm():
+    style_path = "./hibm.mplstyle"
+
+    # Getting all the anaconda enviroments.
+    home = os.getenv("HOME")
+    envs_path = os.path.join(home, "anaconda3/envs/")
+    envs = os.listdir(envs_path)
+    envs = [env for env in envs if "." not in env]
+
+    print(f"Found {len(envs)} environments:")
+    for env in envs:
+        print("  -", env)
+    print("")
+
+    # Constructing the matplotlib paths
+    mpl_paths = [os.path.join(envs_path, env, "lib") for env in envs]
+    mpl_paths
+
+    for i, path in enumerate(mpl_paths):
+        folders = os.listdir(path)
+
+        for folder in folders:
+            if "python" == folder[:6]:
+                path = os.path.join(path, folder)
+                mpl_paths[i] = path
+                break
+
+    mpl_paths = [os.path.join(path, "site-packages") for path in mpl_paths]
+
+    # Start copying the styles into each env.
+    print("Copying the style into each environment...")
+    for i, path in enumerate(mpl_paths):
+        path = os.path.join(path, "matplotlib/mpl-data/stylelib/")
+
+        if os.path.exists(path):
+            copy(style_path, path)
+            print(f"Style copied into {envs[i]}")
+        else:
+            print(f"Error: matplotlib path not found in {envs[i]} env")
